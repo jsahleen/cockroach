11ac6883714a9d4c25d1dae58073453be7f48d26
Joel Sahleen <jsahleen@adobe.com>
2014-06-10 20:18:33 -0600

    updating sql script for mysql compatibility.

--- scripts/reset.sql
+++ scripts/reset.sql
@@ -6,15 +6,15 @@ CREATE TABLE bugs (
 	title CHAR(200) NOT NULL,
 	description TEXT,
 	status ENUM('New', 'Confirmed', 'In Process', 'Resolved', 'Verified'),
-	created DATETIME NOT NULL DEFAULT NOW(),
-	modified DATETIME NOT NULL DEFAULT NOW())
+	created DATETIME NOT NULL,
+	modified DATETIME NOT NULL)
 	ENGINE = InnoDB,
 	DEFAULT CHARACTER SET = 'utf8';
 
 CREATE TABLE change_log (
 	id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	bug_id MEDIUMINT NOT NULL,
-	change_date DATETIME NOT NULL DEFAULT NOW(),
+	change_date DATETIME NOT NULL,
 	from_status ENUM('New', 'Confirmed', 'In Process', 'Resolved', 'Verified'),
 	to_status ENUM('New', 'Confirmed', 'In Process', 'Resolved', 'Verified')) 
 	ENGINE = InnoDB,
